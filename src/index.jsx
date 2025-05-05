2df5d4aa91145678b268e5a7ba51c1026129798d
User Friendly <pivanov@user-friendly.dev>
2025-05-04 19:13:34 -0500

    Setup Vite library mode.

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+
+// Don't user * wildcard to export - it does not export defaults and is weird in general.
+
+export {default as CommonMarkdown}		from '#Components/CommonMarkdown'
+export {default as Form}			 	from '#Components/Form'
+export {default as TextInput}			from '#Components/TextInput'
+export {default as LegaleseMarkdown}	from '#Components/LegaleseMarkdown'
+export {default as RouteLink}			from '#Components/RouteLink'
+export {default as Spinner}				from '#Components/Spinner'
+
+export {
+	default as Icon,
+	IconDecorative,
+	IconClean
+}										from '#Components/Icon'
+
+export {
+	Section,
+	Paragraph,
+	Heading,
+}										from '#Components/Text'
